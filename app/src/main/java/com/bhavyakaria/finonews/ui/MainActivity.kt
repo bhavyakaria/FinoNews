c7cd549d595a0d393562f7cbb91ca7581d9f065d
bhavyakaria <bhavyakaria@yahoo.in>
2019-12-15 18:49:46 +0530

    added splash theme

--- app/src/main/java/com/bhavyakaria/finonews/ui/MainActivity.kt
+++ app/src/main/java/com/bhavyakaria/finonews/ui/MainActivity.kt
@@ -16,6 +16,7 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
